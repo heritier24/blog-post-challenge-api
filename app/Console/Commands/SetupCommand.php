1384230b6286f9648269c13d036703b8d77c4b0c
heritier24 <heritier.ganza@kudibooks.com>
2023-08-29 01:57:16 +0200

    default user command and default post blog created well

--- app/Console/Commands/SetupCommand.php
+++ app/Console/Commands/SetupCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Posts;
 use App\Models\User;
 use App\Services\BlogsServices;
 use Illuminate\Console\Command;
@@ -29,12 +30,19 @@ class SetupCommand extends Command
     {
         // create default user author
         $name = "Ion Plus Technology ";
-        $email = "admin1@gmail.com";
+        $email = "admin@gmail.com";
         $password = "123456";
 
         $user = $service->creatingAuthor($name, $email, $password);
 
-        $this->info("Defalt user created successfully");
+        Posts::create([
+            "titles" => "default title",
+            "content" => "default content",
+            "published_at" => "default published",
+            "author_id" => 1
+        ]);
+
+        $this->info("Defalt user and post created successfully");
         return $user;
     }
 }
